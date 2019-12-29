1834eb172bb3c9cc9b323e4ff6e3135b856a5f54
Amir <naam03@mail.ru>
2019-12-29 19:26:12 +0300

    Update settings.py

--- yandex_courses_django/yandex_courses_django/settings.py
+++ yandex_courses_django/yandex_courses_django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rk6137xm$$s#_*-y6@yhke=i^n2@^b+lscxrdtsgs!wo4j6s$m'
+SECRET_KEY = 'none'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
